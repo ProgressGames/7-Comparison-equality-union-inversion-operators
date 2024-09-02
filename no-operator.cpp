9f5b93f9f98fa0d45300225d68a56e358b773533
Terrarich <163340619+terrarich@users.noreply.github.com>
2024-09-02 22:23:55 +0300

    Update and rename main.cpp to no-operator.cpp

---
+++ no-operator.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+void main()
+{
+    cout << !(4 == 5 ) << endl;
+}
+//Как вы видите прмиер не правильный, но ! делает ответ противоположным, тоесть вместо 0(false) будет 1(true)
+//As you can see, the order is not correct, but ! makes the answer the opposite, that is, instead of 0(false) it will be 1(true)
