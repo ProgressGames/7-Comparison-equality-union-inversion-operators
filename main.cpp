8447bfb3dbf8d8e0f893270a44442b2ecc5849f0
Terrarich <163340619+terrarich@users.noreply.github.com>
2024-09-02 22:19:59 +0300

    Add "and" operator

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+void main()
+{
+    cout << ((5 != 5)&& (4 < 5)) << endl;
+}
+
+//Если весь пример правильный, то будет 1(true), а если весь пример не правильный, то будет 0(false)
