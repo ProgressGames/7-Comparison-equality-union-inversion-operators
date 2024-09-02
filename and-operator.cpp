6cce1eabc6667cf3b071ea51faca8da79c76d139
Terrarich <163340619+terrarich@users.noreply.github.com>
2024-09-02 22:20:33 +0300

    Update and rename main.cpp to and-operator.cpp

---
+++ and-operator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+void main()
+{
+    cout << ((5 != 5)&& (4 < 5)) << endl;
+}
+
+//Если весь пример правильный, то будет 1(true), а если весь пример не правильный, то будет 0(false)
+//If the whole example is correct, it will be 1(true), and if the whole example is not correct, it will be 0(false)
